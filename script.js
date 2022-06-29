00b6cb11304f1dcd4194fd90506b1964626e21af
kelchan <kelvin.chan131@gmail.com>
2022-06-29 15:27:06 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function addShadow( element ) {
+    element.classList.add( 'shadow' );
+}
+
+function removeShadow( element ) {
+    element.classList.remove( 'shadow' );
+}
+
+function addWhiteShadow( element ) {
+    element.classList.add( 'white-shadow' );
+}
+
+function removeWhiteShadow( element ) {
+    element.classList.remove( 'white-shadow' );
+}
+
